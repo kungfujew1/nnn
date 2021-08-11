782211d15b81d9a97bfb5a2d7c310f57a3595585
kungfujew <kungfujew@DESKTOP-L1NPK93>
2021-08-11 11:58:58 -0400

    ffdf
    ffd

---
+++ HenryMod/HenryPlugin.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using HenryMod.Modules.Survivors;
+using R2API.Utils;
+using RoR2;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace HenryMod
+{
+    [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [BepInPlugin(MODUID, MODNAME, MODVERSION)]
+    [R2APISubmoduleDependency(new string[]
+    {
+        "PrefabAPI",
+        "LanguageAPI",
+        "SoundAPI",
+    })]
+
+    public class HenryPlugin : BaseUnityPlugin
+    {
+        // if you don't change these you're giving permission to deprecate the mod-
+        //  please change the names to your own stuff, thanks
+        //   this shouldn't even have to be said
+        public const string MODUID = "com.KungandDev.BOTWLink";
+        public const string MODNAME = "BOTWLink";
+        public const string MODVERSION = "1.0.0";
+
+        // a prefix for name tokens to prevent conflicts- please capitalize all name tokens for convention
+        public const string developerPrefix = "ROB";
+
+        internal List<SurvivorBase> Survivors = new List<SurvivorBase>();
+
+        public static HenryPlugin instance;
+
+        private void LateSetup(HG.ReadOnlyArray<RoR2.ContentManagement.ReadOnlyContentPack> obj)
+        {
+            // have to set item displays later now because they require direct object references..
+            Modules.Survivors.MyCharacter.instance.SetItemDisplays();
+        }
+
+        private void Hook()
+        {
+            // run hooks here, disabling one is as simple as commenting out the line
+            On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
+        }
+
+        private void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            orig(self);
+
+            // a simple stat hook, adds armor after stats are recalculated
+            if (self)
+            {
+                if (self.HasBuff(Modules.Buffs.armorBuff))
+                {
+                    self.armor += 300f;
+                }
+            }
+        }
+    }
+}
---
+++ HenryMod/Modules/States.cs
@@ -0,0 +1,24 @@
+﻿using HenryMod.SkillStates;
+using HenryMod.SkillStates.BaseStates;
+using System;
+using System.Collections.Generic;
+
+namespace HenryMod.Modules
+{
+    public static class States
+    {
+        internal static List<Type> entityStates = new List<Type>();
+
+        internal static void RegisterStates()
+        {
+            entityStates.Add(typeof(BaseMeleeAttack));
+            entityStates.Add(typeof(SlashCombo));
+
+            entityStates.Add(typeof(Shoot));
+
+            entityStates.Add(typeof(Roll));
+
+            entityStates.Add(typeof(ThrowBomb));
+        }
+    }
+}
